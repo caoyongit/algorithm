c35bd01720c9f8f86216293da5bb71152b71efa9
yong cao <caoyongit2007@gmail.com>
2021-03-20 21:00:49 +0800

    判断单链表为有环链表

--- JosephRing.c
+++ JosephRing.c
@@ -56,7 +56,77 @@ void findAndKillK(person*head, int k, int m)
 	free(p);
 }
 
+typedef struct Link {
+	char elem;//代表数据域
+	struct Link *next;//代表指针域，指向直接后继元素
+}link;//link为节点名，每个节点都是一个link结构体
 
+typedef enum bool
+{
+	False = 0,
+	True = 1
+}bool;
+
+/*
+从给定链表的第一个节点开始遍历，每遍历至一个节点，
+都将其和所有的前驱节点进行比对，如果为同一个节点，
+则表明当前链表中有环；反之，如果遍历至链表最后一个节点，
+仍未找到相同的节点，则证明该链表中无环。
+*/
+// H为链表的表头
+bool haveRing(link * H)
+{
+	link * Htemp = H;
+	// 存储所遍历节点所有前驱节点的存储地址，64位环境下地址占8个字节，所以这里用long long类型
+	long long addr[20] = { 0 };
+	int length = 0, i = 0;
+	//桌个遍历链表中各个节点
+	while (Htemp) {
+		//依次将Htemp和addr数组中记录的已遍历的地址进行比对
+		for (i = 0; i < length; i++) {
+			//如果比对成功，则证明有环
+			if (Htemp == addr[i]) {
+				return True;
+			}
+		}
+		//比对不成功，则记录Htemp节点的存储地址
+		addr[length] = Htemp;
+		length++;
+		Htemp = Htemp->next;
+	}
+	return False;
+}
+
+/*
+即在一个链表中，如果 2 个指针（假设为 H1 和 H2）都从表头开始遍历链表，
+其中 H1 每次移动 2 个节点的长度（H1 = H1->next->next）,
+而 H2 每次移动 1 个节点的长度（H2 = H2->next），如果该链表为有环链表，
+则 H1、H2 最终必定会相等；反之，如果该链表中无环，则 H1、H2 永远不会相遇。
+*/
+bool ifHaveRing(link * H) {
+	link * H1 = H->next;
+	link * H2 = H;
+	while (H1) {
+		if (H1 = H2) {
+			//链表中有环
+			return True;
+		}
+		else {
+			H1 = H1->next;
+			if (!H1) {
+				//链表中无环
+				return False;
+			}
+			else {
+				H1 = H1->next;
+				H2 = H2->next;
+			}
+		}
+	}
+	return False;
+}
+
+/*
 int main()
 {
 	printf("输入圆桌上的人数n:");
@@ -71,4 +141,4 @@ int main()
 	scanf("%d", &m);
 	findAndKillK(head, k, m);
 	return 0;
-}
+}*/
